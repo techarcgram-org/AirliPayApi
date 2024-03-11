27a98484d698d9dfea65ec92cc6c21386864d74c
spycode1 <tafodinho@gmail.com>
2024-03-11 15:35:28 +0100

    update docker plugin jenkins

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
+echo 'The following "yarn" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
-npm run build
+yarn run build
 set +x
 
-echo 'The following "npm" command runs your Node.js/React application in'
+echo 'The following "yarn" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
+echo 'The "yarn start" command has a trailing ampersand so that the command runs'
 echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+echo 'can pause running builds of CI/CD applications indefinitely. "yarn start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
+echo 'of the previously run process (i.e. "yarn start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+yarn start &
 sleep 1
 echo $! > .pidfile
 set +x
