16ee6a7a2d7a596924838b39b4a916a39e265505
Nde Lucien <chelucien08@gmail.com>
2024-07-17 18:06:37 +0100

    lint --fix

--- src/modules/savings-balance/dto/update-savings-balance.dto.ts
+++ src/modules/savings-balance/dto/update-savings-balance.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateSavingsBalanceDto } from './create-savings-balance.dto';
 
-export class UpdateSavingsBalanceDto extends PartialType(CreateSavingsBalanceDto) {}
+export class UpdateSavingsBalanceDto extends PartialType(
+  CreateSavingsBalanceDto,
+) {}
