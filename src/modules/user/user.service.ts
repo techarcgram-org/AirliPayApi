f4875d2dbaceb9fd7ec672fa4551e8e874ac03c4
Nde Lucien <chelucien08@gmail.com>
2024-09-04 15:17:11 +0100

    hey fix

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -868,7 +868,7 @@ export class UserService {
         const lastInvoice = invoices?.[0];
         const currentDate = new Date();
         const newInvoice = {
-          from: lastInvoice > 0 ? lastInvoice.to : user.created_at,
+          from: invoices.length > 0 ? lastInvoice.to : user.created_at,
           to: currentDate,
           transactions: [],
         };
